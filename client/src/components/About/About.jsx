ca96b57f1072aa86205ca75b7102bc6527a72851
unknown <noman.khan3701@gmail.com>
2022-06-25 01:55:22 +0530

    Added about section on home page

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './About.scss'
+import Lottie from 'react-lottie-player'
+import gardeningJson from '../../assets/lottie/gardening.json'
+import contactJson from '../../assets/lottie/contact-us-lottie.json'
+import deliveryJson from '../../assets/lottie/delivery-bike.json'
+import envelopeJson from '../../assets/lottie/nature-envelope.json'
+
+const About = () => {
+	return (
+		<div className='about'>
+			<div className="about-info">
+				<div className="image">
+					<Lottie
+						loop
+						animationData={gardeningJson}
+						play
+						style={{ margin: 'auto', height: '250px', minHeight: '200px' }}
+					/>
+				</div>
+				<div className="content-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam excepturi minima, molestias quia minus non quasi aliquam vero debitis? Autem praesentium consequuntur voluptas consequatur soluta odio modi, aut ut optio.</div>
+			</div>
+			<div className="about-info">
+				<div className="content-left">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis eius deserunt dignissimos quod id ducimus suscipit voluptatum est porro nihil, cum fugiat, quidem dolores aperiam? Illo accusantium ad inventore. Exercitationem?</div>
+				<div className="image">
+					<Lottie
+						loop
+						animationData={contactJson}
+						play
+						style={{ margin: 'auto', height: '250px', minHeight: '200px' }}
+					/>
+				</div>
+			</div>
+			<div className="about-info">
+				<div className="image">
+					<Lottie
+						loop
+						animationData={deliveryJson}
+						play
+						style={{ margin: 'auto', height: '250px', minHeight: '200px' }}
+					/>
+				</div>
+				<div className="content-right">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit dolorum vitae tempore dicta, minus unde laboriosam incidunt, alias accusamus, aperiam expedita voluptatibus illum tenetur vel quae voluptas hic enim natus.</div>
+			</div>
+			<div className="about-info">
+				<div className="content-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam veniam nemo minus, vitae voluptates repudiandae saepe, natus molestias cupiditate ipsa expedita fuga culpa a esse dolores sapiente adipisci enim odit!</div>
+				<div className="image">
+					<Lottie
+						loop
+						animationData={envelopeJson}
+						play
+						style={{ margin: 'auto', height: '250px', minHeight: '200px' }}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default About
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Header } from '../../components/import'
+import { About, Header } from '../../components/import'
 import './Home.scss'
 
 const Home = () => {
 	return (
 		<div className='home container'>
 			<Header />
+			<About />
 		</div>
 	)
 }
