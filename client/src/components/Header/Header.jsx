60563c14a6dab5018f95a1df9b7c1262218c0f3d
unknown <noman.khan3701@gmail.com>
2022-06-24 23:25:16 +0530

    Creating header section

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import "./Header.scss";
 
 const Header = () => {
-	return (
-		<div>Header</div>
-	)
-}
+  return (
+    <div className="header">
+      <div className="bg-sections">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <div className="header-info container">
+        <div className="left">
+          <div className="title">
+            <div className="green">Together</div>
+			<div>We can restore the planet</div>
+          </div>
+        </div>
+        <div className="right">sdfgkjnsfg</div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
