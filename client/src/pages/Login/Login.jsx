c19e09b44862b6a2e78818dab781f7df967df5b0
unknown <noman.khan3701@gmail.com>
2022-06-25 01:04:29 +0530

    Got the code from shreyash

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
-import './Login.scss'
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import FullScreenLoader from "../Signup/FullScreenLoader";
+import "react-toastify/dist/ReactToastify.css";
+import "./Login.scss";
+
+// const admin_server_url = import.meta.env.ADMIN_SERVER_URL;
+// const admin_server_url = process.env.REACT_APP_SERVER_URL;
+const admin_server_url = "http://localhost:8000/api";
 
 const Login = () => {
-  return (
-	<div className='container'>Login</div>
-  )
-}
+  const [isLoading, setLoading] = useState(false);
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  if (isLoading) {
+    return <FullScreenLoader />;
+  }
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setLoginData((prevData) => {
+      return { ...prevData, [name]: value };
+    });
+  };
+
+  const submit = async () => {
+    if (!(loginData.email && loginData.password)) {
+      toast.error("Fields cannot be empty.", { position: "top-center" });
+    } else {
+      setLoading(true);
+      console.log(admin_server_url);
+      const { data: res } = await axios.post(`${admin_server_url}/auth/login`, {
+        email: loginData.email,
+        password: loginData.password,
+      });
+      localStorage.setItem("token", res.data);
+      console.log(res);
+    }
+  };
+  return (
+    <div className="login-container">
+      <ToastContainer></ToastContainer>
+      <div className="login">
+        <h1>Login</h1>
+        <input
+          type="text"
+          name="email"
+          value={loginData.email}
+          placeholder="Email"
+          onChange={onChange}
+        ></input>
+        <input
+          type="password"
+          name="password"
+          value={loginData.password}
+          placeholder="Password"
+          onChange={onChange}
+        ></input>
+        <div className="btn" onClick={submit}>
+          Submit
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
