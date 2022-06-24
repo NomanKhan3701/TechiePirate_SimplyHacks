2175af4d9d93272657673cf052271c08a4869386
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2022-06-25 02:17:58 +0530

    some commit

--- client/src/pages/Signup/FullScreenLoader.jsx
+++ client/src/pages/Signup/FullScreenLoader.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./FullScreenLoader.scss";
 
 const fullScreenLoader = () => {
-<<<<<<< HEAD
   return (
     <div className="full-screen-loader">
       <div className="gooey">
@@ -18,20 +17,3 @@ const fullScreenLoader = () => {
 };
 
 export default fullScreenLoader;
-=======
-	return (
-		<div className="full-screen-loader">
-			<div className="gooey">
-				<span className="dot"></span>
-				<div className="dots">
-					<span></span>
-					<span></span>
-					<span></span>
-				</div>
-			</div>
-		</div>
-	);
-};
-
-export default fullScreenLoader;
->>>>>>> 35c10c2d7530bd204a086d72b36bac588ed09ab4
