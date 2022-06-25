05c9381c2ac330dfb5892988838adf59b2e79bc1
Varun Koranne <88835482+Varun-Dhruv@users.noreply.github.com>
2022-06-25 20:27:21 +0530

    Added features

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const {
-    getPosts,
-    addPost,
-    deletePost,
-    addComment,
-    getComments,
-    deleteComment,
-    getPost, } = require("../controllers/Posts")
+  getPosts,
+  addPost,
+  deletePost,
+  addComment,
+  getComments,
+  deleteComment,
+  getPost,
+} = require("../controllers/Posts");
 const passport = require("passport");
 require("../strategy/jwt-auth");
 
-router.get("/", getPosts)
-router.post("/", passport.authenticate('jwt', { session: false }), addPost)
-router.delete("/", passport.authenticate('jwt', { session: false }), deletePost)
+router.get("/", getPosts);
+router.post("/", passport.authenticate("jwt", { session: false }), addPost);
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  deletePost
+);
 router.get("/getpost", getPost);
-router.get("/Comments", getComments)
-router.post('/Comments', passport.authenticate('jwt', { session: false }), addComment)
-router.delete("/Comments", passport.authenticate('jwt', { session: false }), deleteComment)
+router.get("/Comments", getComments);
+router.post(
+  "/Comments",
+  passport.authenticate("jwt", { session: false }),
+  addComment
+);
+router.delete(
+  "/Comments",
+  passport.authenticate("jwt", { session: false }),
+  deleteComment
+);
 module.exports = router;
