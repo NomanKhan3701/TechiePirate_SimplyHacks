3e7faa9cf76010d7f5e3f209d41198d3a79cbedf
Aditya Kharote <aditya.kharote@spit.ac.in>
2022-06-25 22:03:52 +0530

    Added comments

--- client/src/components/import.js
+++ client/src/components/import.js
@@ -8,6 +8,7 @@ export { default as FileUpload } from './FileUpload/FileUpload'
 export { default as ImgUpdateUpload } from './ImgUpdateUpload/ImgUpdateUpload'
 export { default as CreatePost } from "./CreatePost/CreatePost"
 export { default as Button } from './Button/Button'
+export { default as BigButton } from './BigButton/BigButton'
 export { default as Map } from './Map/Map'
 export { default as MarkdownEditor } from './MarkdownEditor/MarkdownEditor'
 export { default as LimitChar } from './LimitChar/LimitChar'
