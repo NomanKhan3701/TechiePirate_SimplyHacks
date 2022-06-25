eab083d4091695dfc5d8c11679d5fae51ef35fa8
Varun Koranne <88835482+Varun-Dhruv@users.noreply.github.com>
2022-06-25 22:30:03 +0530

    commit

--- server/controllers/Events.js
+++ server/controllers/Events.js
@@ -38,17 +38,23 @@ const createEvent = async (req, res, next) => {
       img: img,
     });
     data.image = url.data.url;
+    data.organizerEmail = req.user.email;
     const { error } = validateEvent(data);
-    res.send("createEvents");
+    if (error)
+      return res.status(400).send({
+        message: error.details[0].message,
+      });
+    const event = await prisma.Events.create({
+      data: data,
+    });
+    // const events = await prisma.Events.findMany({});
+    res.status(201).send({message:"created event successfully",event});
   } catch (error) {}
 };
 const deleteEvent = async (req, res, next) => {
   try {
-
     res.send("deleteEvents");
-  } catch (error) {
-    
-  }
+  } catch (error) {}
 };
 const editEvent = async (req, res, next) => {
   try {
--- server/models/Events.js
+++ server/models/Events.js
@@ -6,7 +6,14 @@ const validateEvent = (data) => {
 };
 const validateParticicpant = (data) => {
   const schema = Joi.object({
-    Image:Joi.string().label("Image")
+    image: Joi.string().label("image"),
+    title: Joi.string().required().label("title"),
+    description: Joi.string().required().label("description"),
+    time: Joi.date().required().label("Date/Time"),
+    city: Joi.string().required().label("City"),
+    address: Joi.string().required().label("Address"),
+    eventTags: Joi.array().items(Joi.string()).label("Tags"),
+    organizerEmail: Joi.string().email().required().label("Email"),
   });
   return schema.validate(data);
 };
