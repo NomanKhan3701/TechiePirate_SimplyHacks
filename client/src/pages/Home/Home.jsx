970e4dcda296510bae5cd2d067d4ff6ad3c79733
Aditya Kharote <aditya.kharote@spit.ac.in>
2022-06-26 11:33:39 +0530

    Profile

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
 import EventCard from '../../components/EventCard/EventCard'
-import { About, Header, Post } from '../../components/import'
+import { About, Header, Loader, Post } from '../../components/import'
 import './Home.scss'
 
+const server_url = process.env.REACT_APP_server_url
+
 const Home = () => {
+	const [posts, setPosts] = useState(null);
+	const [events, setEvents] = useState(null);
+
+	const getPosts = async () => {
+		try {
+			const res = await axios.get(`${server_url}/api/posts`);
+			setPosts(res.data)
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	const getEvents = async () => {
+		try {
+			/*const res = await axios.get(`${server_url}/api/events`);
+			setEvents(res.data)*/
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	useEffect(() => {
+		getPosts();
+		getEvents();
+	}, [])
+
 	return (
 		<div className='home container'>
 			<Header />
@@ -19,6 +48,9 @@ const Home = () => {
 			<div className='home-sec'>
 				<h1>Top Posts</h1>
 				<div className='items-grid'>
+					{!posts ? <div className='loading'><Loader /></div> : posts?.map((post, key) => {
+						return <Post post={post} key={key} />
+					})}
 				</div>
 			</div>
 		</div>
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import { getDefaultPhoto } from '../../utils'
 import './Profile.scss'
+import { AiFillMail, AiFillPhone } from 'react-icons/ai'
+import { TbPlant2 } from 'react-icons/tb'
+import { FaDonate } from 'react-icons/fa'
 
 const Profile = () => {
 	const auth = useAuth()
+	
 	return (
 		<div className='container profile-container page'>
 			<div className='profile-cols'>
@@ -13,14 +17,46 @@ const Profile = () => {
 
           <div className='profile-info'>
             <span>
-              <span></span>
+							<i><AiFillMail></AiFillMail></i>
+              <span>adityakharote@hotmail.com</span>
+            </span>
+
+            <span>
+							<i><AiFillPhone></AiFillPhone></i>
+              <span>+91 9876543210</span>
             </span>
           </div>
         </div>
 
         <div className="right">
           <h1>Aditya Kharote</h1>
-          <div className='profile-content'>Hi
+          <div className='profile-content'>
+						<div className='scores'>
+							<div>
+								<TbPlant2></TbPlant2>
+								<span>0</span>
+							</div>
+							
+							<div>
+								<FaDonate></FaDonate>
+								<span>0</span>
+							</div>
+						</div>
+
+						<div className='profile-sec'>
+							<h3>Events Joined</h3>
+							<div className='pitems-grid'>
+
+							</div>
+						</div>
+
+						<div className='profile-sec'>
+							<h3>Donations</h3>
+							<div className='pitems-grid'>
+								
+							</div>
+						</div>
+
           </div>
         </div>
       </div>
